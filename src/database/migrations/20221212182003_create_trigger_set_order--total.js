4d579d057876509467a684df83cf882cf3aba24d
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2022-12-15 17:03:53 -0300

    fix triggers

--- src/database/migrations/20221212182003_create_trigger_set_order--total.js
+++ src/database/migrations/20221212182003_create_trigger_set_order--total.js
@@ -5,30 +5,38 @@
 exports.up = function(knex) {
     return Promise.all([
         knex.raw(
-            "CREATE TRIGGER `tg_update_order--total_new-item` BEFORE INSERT ON OrderItem "+
+            "CREATE TRIGGER `tg_update_order--total_new-item` AFTER INSERT ON OrderItem "+
             "FOR EACH ROW "+
             "BEGIN "+
             "UPDATE `Order` SET `total` = `total` + (NEW.`price` * NEW.`quantity`) WHERE `id` = NEW.`order`; "+
             "END"
         ),
         knex.raw(
-            "CREATE TRIGGER `tg_update_order--total_update-item-increase` BEFORE UPDATE ON `OrderItem` "+
+            "CREATE TRIGGER `tg_update_order--total_update-item` AFTER UPDATE ON `OrderItem` "+
             "FOR EACH ROW "+
-            "WHEN NEW.deletedAt IS NULL "+
+            "WHEN OLD.`deletedAt` IS NULL AND NEW.`deletedAt` IS NULL "+
             "BEGIN "+
-            "UPDATE `Order` SET `total` = `total` + (NEW.`price` * NEW.`quantity`) WHERE `id` = NEW.`order`; "+
+            "UPDATE `Order` SET `total` = `total` - (OLD.`price` * OLD.`quantity`) + (NEW.`price` * NEW.`quantity`) WHERE `id` = NEW.`order` AND NEW.`order` = OLD.`order`; "+
             "END"
         ),
         knex.raw(
-            "CREATE TRIGGER `tg_update_order--total_update-item-decrease` BEFORE UPDATE ON `OrderItem` "+
+            "CREATE TRIGGER `tg_update_order--total_update-item-insert` AFTER UPDATE ON `OrderItem` "+
             "FOR EACH ROW "+
-            "WHEN NEW.deletedAt IS NOT NULL "+
+            "WHEN OLD.`deletedAt` IS NOT NULL AND NEW.`deletedAt` IS NULL "+
             "BEGIN "+
-            "UPDATE `Order` SET `total` = `total` - (OLD.`price` * OLD.`quantity`) WHERE `id` = OLD.`order`; "+
+            "UPDATE `Order` SET `total` = `total` + (NEW.`price` * NEW.`quantity`) WHERE `id` = NEW.`order` AND NEW.`order` = OLD.`order`; "+
             "END"
         ),
         knex.raw(
-            "CREATE TRIGGER `tg_update_order--total_delete-item` BEFORE DELETE ON `OrderItem` "+
+            "CREATE TRIGGER `tg_update_order--total_update-item-delete` AFTER UPDATE ON `OrderItem` "+
+            "FOR EACH ROW "+
+            "WHEN OLD.`deletedAt` IS NULL AND NEW.`deletedAt` IS NOT NULL "+
+            "BEGIN "+
+            "UPDATE `Order` SET `total` = `total` - (OLD.`price` * OLD.`quantity`) WHERE `id` = OLD.`order` AND NEW.`order` = OLD.`order`; "+
+            "END"
+        ),
+        knex.raw(
+            "CREATE TRIGGER `tg_update_order--total_delete-item` AFTER DELETE ON `OrderItem` "+
             "FOR EACH ROW "+
             "BEGIN "+
             "UPDATE `Order` SET `total` = `total` - (OLD.`price` * OLD.`quantity`) WHERE `id` = OLD.`order`; "+
